c0bff52ef01be8dfa62d8983479db1ca9aa63642
Valentin <valentin.gassend@gmail.com>
2025-05-30 10:59:01 +0200

    wip: screen

--- src/Config/TemplateManager.js
+++ src/Config/TemplateManager.js
@@ -392,29 +392,29 @@ class TemplateManager {
                 group: 'screen'
             },
             // Screens
-            'Cube001_1': {
-                id: 'ScreenOldEmission',
-                path: '/models/digital/screen/ScreenOldEmission.glb',
-                priority: 21,
-                useTextures: false,
-                group: 'screen'
-            },
-
-            // 'Cube001_3': {
-            //     id: 'ScreenEmission',
-            //     path: '/models/digital/screen/ScreenEmission.glb',
-            //     priority: 22,
+            // 'Cube001_1': {
+            //     id: 'ScreenOldEmission',
+            //     path: '/models/digital/screen/ScreenOldEmission.glb',
+            //     priority: 21,
             //     useTextures: false,
             //     group: 'screen'
             // },
-            // // Screens
-            'mesh002_1': {
-                id: 'ScreenOld',
-                path: '/models/digital/screen/ScreenOld.glb',
-                priority: 21,
-                useTextures: true,
+
+            'Cube001_3': {
+                id: 'ScreenEmission',
+                path: '/models/digital/screen/ScreenEmission.glb',
+                priority: 22,
+                useTextures: false,
                 group: 'screen'
             },
+            // // Screens
+            // 'mesh002_1': {
+            //     id: 'ScreenOld',
+            //     path: '/models/digital/screen/ScreenOld.glb',
+            //     priority: 21,
+            //     useTextures: true,
+            //     group: 'screen'
+            // },
             // // 'BranchEucalyptus_2': {
             // //     id: 'BranchEucalyptusScreen',
             // //     path: '/models/forest/branch/BranchEucalyptus.glb',
